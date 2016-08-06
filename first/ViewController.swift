be5bf227d6d610f5f03a71f5866ffc8e9258a56b
Mantra iMac <mantraideas@gmail.com>
2016-08-06 11:39:29 +0545

    first commit

---
+++ first/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Mantra Ideas on 8/6/16.
+//  Copyright © 2016 Mantra Ideas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var myButton: UIButton!
+    
+    @IBOutlet weak var callButton: UIButton!
+    
+    @IBOutlet weak var msgButton: UIButton!
+//    private var myButton1:UIButton=UIButton()
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        msgButton.addTarget(self, action: #selector(ViewController.message), forControlEvents: .TouchDown)
+        callButton.addTarget(self, action: #selector(ViewController.makeCalled), forControlEvents: .TouchUpInside)
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+    @IBAction func openDialog(sender: AnyObject) {
+        
+        let dialog:UIAlertController = UIAlertController(title: "Hello",message: "How are you?",
+                                       preferredStyle: UIAlertControllerStyle.Alert)
+       
+        presentViewController(dialog,animated: false, completion: nil)
+    }
+    
+    func makeCalled(){
+        print("Button is Clicked")
+    }
+    func message() -> String {
+        
+        return "";
+    }
+}
+
